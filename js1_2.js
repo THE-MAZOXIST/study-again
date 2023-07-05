921e489496aa811d7ae6950373572826c867a1f8
Maksim Birulia <birula6666@gmail.com>
2023-07-05 22:59:18 +0300

    garbage cleaning

--- js1_2.js
+++
@@ -1,9 +0,0 @@
-const message = 'Hello world' // Try edit me
-var name = prompt('кто ты?', );
-
-// Update header text
-document.querySelector('#header').innerHTML = 'ну здравствуй,  ' + name
-
-
-// Log to console
-console.log(message)
